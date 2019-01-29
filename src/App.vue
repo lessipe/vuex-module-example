7bc23e43b0d3540913ae2b4443315530f1ed2954
djso <djso@lessipe.com>
2019-01-29 20:51:36 +0900

    module init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>
+      module1
+    </h1>
+    <h2>{{ module1ValueWithHello }}</h2>
+    <br />
+
+    module1 값 변경
+    <input type="text" v-model="module1Value">
+    <button type="button" @click="changeModule1Value">변경</button>
+    <br />
+    <h1>
+      module2
+    </h1>
+    <h2>{{ module2ValueWithHello }}</h2>
+    module2 값 변경
+    <input type="text" v-model="module2Value">
+    <button type="button" @click="changeModule2Value">변경</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { mapGetters } from 'vuex';
 
 export default {
   name: 'app',
+  data() {
+    return {
+      module1Value: '',
+      module2Value: ''
+    };
+  },
+  methods: {
+    changeModule1Value() {
+      this.$store.dispatch('setModule1Value', this.module1Value);
+    },
+    changeModule2Value() {
+      this.$store.dispatch('setModule2Value', this.module2Value);
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'module1ValueWithHello',
+      'module2ValueWithHello'
+    ])
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/store/index.js
@@ -0,0 +1,55 @@
+import Vuex from 'vuex';
+import Vue from 'vue';
+
+Vue.use(Vuex);
+
+const module1 = {
+  state: {
+    module1Value: 'Module 1'
+  },
+  getters: {
+    module1ValueWithHello(state, getters) {
+      return 'Hello, ' + state.module1Value;
+    }
+  },
+  mutations: {
+    setModule1Value(state, value) {
+      state.module1Value = value;
+    }
+  },
+  actions: {
+    setModule1Value(context, value) {
+      context.commit('setModule1Value', value);
+    }
+  }
+};
+
+const module2 = {
+  state: {
+    module2Value: 'Module 2'
+  },
+  getters: {
+    module2ValueWithHello(state, getters) {
+      return 'Hello, ' + state.module2Value;
+    }
+  },
+  mutations: {
+    setModule2Value(state, value) {
+      state.module2Value = value;
+    }
+  },
+  actions: {
+    setModule2Value(context, value) {
+      context.commit('setModule2Value', value);
+    }
+  }
+};
+
+const store = new Vuex.Store({
+  modules: {
+    module1,
+    module2
+  }
+});
+
+export default store;
